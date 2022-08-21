ffdf9213a11f44bca282b2d3b2e360020d374293
Kekmus <lgstrwrs@gmail.com>
2022-08-21 15:32:16 +0500

    Feat: add link on github repo in footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,10 +1,22 @@
-import style from"./Footer.module.css"
+import style from "./Footer.module.css";
+import { AiFillGithub } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <footer className={style.footer}>
+      <a
+        className={style.github__link}
+        target="_blank"
+        href="https://github.com/Kekmus/eshoop-next"
+        rel="noreferrer"
+      >
+        <AiFillGithub className={style.github__icon} />
+        <p className={style.github__text}>
+          Github
+        </p>
+      </a>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
